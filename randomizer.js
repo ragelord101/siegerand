c5e4299d329cd0478d4eeb38e4fb1e6452400a8a
ragelord101 <JoshiMedina101@gmail.com>
2025-06-14 15:38:37 -0400

    basic framework should be done

--- randomizer.js
+++ randomizer.js
@@ -3,4 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 const path = require('path');
 
+app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
