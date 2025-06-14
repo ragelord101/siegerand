cf5d46dbfed6b81d8c211a729c0fb8c4b1f7cc4a
ragelord101 <JoshiMedina101@gmail.com>
2025-06-14 17:27:26 -0400

    test framework completed, ready for testing

--- randomizer.js
+++ randomizer.js
@@ -7,3 +7,17 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('startmenu')
+})
+app.listen(
+    port,
+    () => console.log(`system is now operational`),
+    console.log(`local testing at http://localhost:${port}`),
+)
+
