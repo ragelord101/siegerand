367c8f0896100c3c8c2ff1d8da954243455bacad
ragelord101 <JoshiMedina101@gmail.com>
2025-06-14 20:57:29 -0400

    shifted around the script file to public

---
+++ public/viewscripts/startcript.js
@@ -0,0 +1,6 @@
+//i need this to gather up the names of all the characters that the person has so that
+//it can send a list of those characters to the randomizer to be used later
+
+function getCharacterNames() {
+    let characterNames = [];
+}
